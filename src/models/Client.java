6c66208ac382fd93d7918c6abbbb23dc5fe1427a
cifelse <meetcif@gmail.com>
2024-02-23 14:45:22 +0800

    Finish Client

--- src/models/Client.java
+++ src/models/Client.java
@@ -7,22 +7,24 @@ public class Client {
     public static void main(String[] args) throws IOException {
         String hostname = "localhost";
         int port = 8080;
+
+        int start = 0;
+        int end = 10;
         
         // Create a socket
         Socket socket = new Socket(hostname, port);
 
         // Create input and output streams
-        DataInputStream in = new DataInputStream(socket.getInputStream());
         DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        DataInputStream in = new DataInputStream(socket.getInputStream());
 
-        // Send a message to the server
-        String message = "Hello from the client!";
-        out.writeUTF(message);
+        // Send the instructions to the master
+        out.writeUTF(start + " " + end);
         out.flush();
 
         // Receive a response from the server
         String response = in.readUTF();
-        System.out.println("Server response: " + response);
+        System.out.println(response);
 
         // Close the connection
         socket.close();
