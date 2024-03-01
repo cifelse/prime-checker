e181f19cebf951d0d9c3a3297849b6fda4a01eba
amndaprz <amanda_perez@dlsu.edu.ph>
2024-03-01 21:07:37 +0800

    Update test.py

--- src/models/test.py
+++ src/models/test.py
@@ -8,7 +8,7 @@ def t_test(array1, array2):
     return t_statistic, p_value
 
 # Example usage
-array1 = np.array([163059.6, 91571.0, 60568.4, 50679.6, 40688.8, 36682.8,37029.0, 38868.2,35673.2, 38854.4,36244.4]) #With Workers
+array1 = np.array([163059.6, 91571.0, 60568.4, 50679.6, 40688.8, 36682.8,37029.0, 38868.2,35673.2, 35854.4,36244.4]) #With Workers
 array2 = np.array([259672.4, 166515.8, 93446.2,63012.8, 54851.0,53030.2,52481.6,51105.6,51983.8,52957.6,53595.6]) #Without workers
 
 t_stat, p_value = stats.ttest_rel(array1, array2)
