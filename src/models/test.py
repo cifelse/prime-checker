9872a8d1dae11fb28d615ff401fd4f026d5eb39b
amndaprz <amanda_perez@dlsu.edu.ph>
2024-03-01 23:25:25 +0800

    Update test.py

--- src/models/test.py
+++ src/models/test.py
@@ -12,8 +12,8 @@ array1 = np.array([163059.6, 91571.0, 60568.4, 50679.6, 40688.8, 36682.8,37029.0
 array2 = np.array([259672.4, 166515.8, 93446.2,63012.8, 54851.0,53030.2,52481.6,51105.6,51983.8,52957.6,53595.6]) #Without workers
 
 t_stat, p_value = stats.ttest_rel(array1, array2)
-print("Null Hypothesis (H0): There is no significant difference between the response times (ms) of the distributed and non-distributed prime checker.")
-print("Alternative Hypothesis (H1): There is a significant difference between the response times (ms) of the distributed and non-distributed prime checker.")
+print("Null Hypothesis (H0): TThere is no significant difference between the response times (ms) of the distributed with slave servers and the prime checker without slave servers.")
+print("Alternative Hypothesis (H1): There is a significant difference between the response times (ms) of the distributed with slave servers and the prime checker without slave servers.")
 print("T-statistic:", round(t_stat, 5))
 print("P-value:", round(p_value, 5))
 
